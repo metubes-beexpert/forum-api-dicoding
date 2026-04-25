31922021b89060f11bc770cc1360a546fd9ad69b
Muhammad Syarifudin <muh.syarifudinnfree@gmail.com>
2026-04-25 17:02:16 +0700

    feat: add breaking test

--- src/Commons/exceptions/_test/ClientError.test.js
+++ src/Commons/exceptions/_test/ClientError.test.js
@@ -1,7 +1,15 @@
-import ClientError from '../ClientError.js';
+import ClientError from "../ClientError.js";
 
-describe('ClientError', () => {
-  it('should throw error when directly use it', () => {
-    expect(() => new ClientError('')).toThrowError('cannot instantiate abstract class');
+describe("ClientError", () => {
+  it("should throw error when directly use it", () => {
+    expect(() => new ClientError("")).toThrowError(
+      "cannot instantiate abstract class"
+    );
+  });
+});
+
+describe("Skenario Gagal CI", () => {
+  it("seharusnya sengaja gagal", () => {
+    expect(1 + 1).toEqual(3);
   });
 });
